e70b6e9739f1dc71dc485c5e6f9f7c89d9b46a37
dhillonajender <97201745+dhillonajender@users.noreply.github.com>
2022-07-08 11:55:56 -0400

    Update settings.py

--- login/settings.py
+++ login/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  
         'NAME': 'practice',  
         'USER': 'root',  
-        'PASSWORD': 'Ajender@950',  
+        'PASSWORD': '',  
         'HOST': '127.0.0.1',  
         'PORT': '3306',  
         'OPTIONS': {  
